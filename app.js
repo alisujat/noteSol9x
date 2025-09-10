d28919f6386fa7667a026b86cc0fd0edf43ae2f6
alisujat <sujat.ali@innobitsystems.com>
2025-09-10 13:04:01 -0700

    export default server instance

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
-import express from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-const app = express()
+// Initialize app
+const app = express();
 
+// Middleware
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
-}))
+}));
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(cookieParser());
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(cookieParser())
+// Routes import
+import userRouter from './src/routes/user.js';
+import noteRouter from "./src/routes/note.js";
 
+// Routes declaration
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/note", noteRouter);
 
-//routes import
-import userRouter from './src/routes/user.js'
-import noteRouter from "./src/routes/note.js"
-
-//routes declaration
-app.use("/api/v1/users", userRouter)
-app.use("/api/v1/note", noteRouter)
-
-export { app }
+// Export default server instance
+export default app;
