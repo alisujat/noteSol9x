5f915358b510f842afeaae7acec5bd2c45b611d2
alisujat <sujat.ali@innobitsystems.com>
2025-09-10 23:48:25 -0700

    removes cookies from middleware

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -5,7 +5,7 @@ import { User } from "../modals/user.js";
 
 export const verifyJWT = asyncHandler(async(req, _, next) => {
     try {
-        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+        const token = req.header("Authorization")?.replace("Bearer ", "")
         
         // console.log(token);
         if (!token) {
@@ -14,7 +14,7 @@ export const verifyJWT = asyncHandler(async(req, _, next) => {
     
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
     
-        const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+        const user = await User.findById(decodedToken?._id).select("-password")
     
         if (!user) {
             
